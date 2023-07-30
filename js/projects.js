080567ccd97217ca75ce9acfd751d84f744c42fd
Javier Ferraro <javiernicolas010101@gmail.com>
2023-07-30 15:25:55 -0300

    projects_v1.1

--- js/projects.js
+++ js/projects.js
@@ -24,3 +24,70 @@ function redirectToProjectPage(event) {
 imageBoxes.forEach(imageBox => {
     imageBox.addEventListener('click', redirectToProjectPage);
 });
+
+
+
+
+
+
+const grayscaleImages = document.querySelectorAll('.grayscale');
+
+// Función para aplicar el filtro de blanco y negro a la imagen
+function applyGrayscale(event) {
+  const image = event.target;
+  image.style.filter = 'grayscale(100%)';
+}
+
+// Función para quitar el filtro de blanco y negro de la imagen
+function removeGrayscale(event) {
+  const image = event.target;
+  image.style.filter = 'none';
+}
+
+// Agregar eventos para detectar cuando el ratón entra y sale de la imagen
+grayscaleImages.forEach(image => {
+  image.addEventListener('mouseenter',removeGrayscale); 
+  image.addEventListener('mouseleave',applyGrayscale);
+});
+
+
+
+
+
+
+
+  
+
+
+
+
+// Obtener todos los elementos con el atributo "data-animation"
+const animatedElements = document.querySelectorAll('[data-animation]');
+
+// Función para comprobar si un elemento está en la ventana visible
+function isInViewport(element) {
+    const bounding = element.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+// Función para aplicar la transición a los elementos en la ventana visible
+function animateElements() {
+    animatedElements.forEach((element) => {
+        if (isInViewport(element)) {
+            // Obtener la clase de animación desde el atributo "data-animation" y aplicarla
+            const animationClass = element.dataset.animation;
+            element.classList.add(animationClass);
+        }
+    });
+}
+
+// Agregar evento de scroll para aplicar la transición cuando los elementos estén en la ventana visible
+window.addEventListener('scroll', animateElements);
+
+// Llamar a la función una vez al cargar la página para animar elementos visibles inicialmente
+animateElements();
