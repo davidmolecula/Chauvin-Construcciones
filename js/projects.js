61aee0917519439613b188b1e95c74604f57bbf5
Javier Ferraro <javiernicolas010101@gmail.com>
2023-08-02 23:11:55 -0300

    projects_v1.6 correciones

--- js/projects.js
+++ js/projects.js
@@ -114,7 +114,7 @@ let reachedEndOfPage = false;
         function redirectToAnotherPageWithDelay(delayTime) {
             loaderElement.classList.add("loader2"); // Iniciar animación del loader
             redirectTimeout = setTimeout(function () {
-                window.location.href = "../projects.html"; // Aquí cambia "otra-pagina.html" por la URL de la página a la que quieres redirigir.
+                window.location.href = "../projects.html"; 
             }, delayTime);
         }
 
@@ -145,29 +145,34 @@ let reachedEndOfPage = false;
 
 
 
-      // Función para verificar si un elemento está en el viewport
+// Función para verificar si un elemento está en el viewport
 function isElementInViewport(element) {
-  const rect = element.getBoundingClientRect();
-  return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
+    var rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    );
 }
 
-// Función para agregar la clase 'aos-animate' a las imágenes cuando estén en el viewport
-function animateImagesOnScroll() {
-  const images = document.querySelectorAll('.animate-image');
-  images.forEach(image => {
-      if (isElementInViewport(image)) {
-          image.classList.add('aos-animate');
-      }
-  });
+// Función para activar la animación de deslizamiento al aparecer en el viewport
+function activateImageAnimations() {
+    var images = document.querySelectorAll('.imagen_animada');
+    images.forEach(function (image) {
+        if (isElementInViewport(image)) {
+            image.classList.add('animate');
+        }
+    });
 }
 
-// Agregar un evento para activar la animación al cargar la página y cuando se haga scroll
-document.addEventListener('DOMContentLoaded', animateImagesOnScroll);
-window.addEventListener('scroll', animateImagesOnScroll);
+// Evento para activar la animación al hacer scroll
+window.addEventListener('scroll', function () {
+    activateImageAnimations();
+});
 
-      
+// Evento para activar la animación al cargar la página
+window.addEventListener('load', function () {
+    activateImageAnimations();
+});
+
+
+
